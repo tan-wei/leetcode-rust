7953ddccedef5efe884525a2b6cffe628d48a67a
Wei Tan <tanwei0404@163.com>
2021-01-09 10:05:44 +0800

    Refactor the test cases to make them clear

--- src/solution/s0001_two_sum.rs
+++ src/solution/s0001_two_sum.rs
@@ -61,14 +61,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_empty_vec() {
-        assert_eq!(Solution::two_sum(vec![], 0), vec![]);
+    fn test_example_1() {
+        let nums: Vec<i32> = vec![2, 7, 11, 15];
+        assert_eq!(Solution::two_sum(nums, 9), vec![0, 1]);
     }
 
     #[test]
-    fn test_normal() {
-        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
-        assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
-        assert_eq!(Solution::two_sum(vec![3, 3], 6), vec![0, 1]);
+    fn test_example_2() {
+        let nums: Vec<i32> = vec![3, 2, 4];
+        assert_eq!(Solution::two_sum(nums, 6), vec![1, 2]);
+    }
+
+    #[test]
+    fn test_example_3() {
+        let nums: Vec<i32> = vec![3, 3];
+        assert_eq!(Solution::two_sum(nums, 6), vec![0, 1]);
+    }
+
+    #[test]
+    fn test_empty_vec() {
+        let nums: Vec<i32> = vec![];
+        assert_eq!(nums, vec![]);
     }
 }
