b8412e6e915452972bdd7e96f307eacd1b03fd0b
Wei Tan <tanwei0404@163.com>
2021-01-25 09:13:45 +0800

    Add a macro support to test Vec without order

--- src/util/testing.rs
+++ src/util/testing.rs
@@ -1,1 +1,18 @@
-
+#[macro_export]
+macro_rules! assert_eq_sorted {
+    ($left:expr, $right:expr) => ({
+        let (mut v1, mut v2) = ($left, $right);
+        v1.sort();
+        v2.sort();
+        assert_eq!(v1, v2)
+    });
+    ($left:expr, $right:expr,) => ({
+        assert_eq_sorted!($left, $right)
+    });
+    ($left:expr, $right:expr, $($arg:tt)+) => ({
+        let (mut v1, mut v2) = ($left, $right);
+        v1.sort();
+        v2.sort();
+        assert_eq!(v1, v2, $($arg)+)
+    });
+}
