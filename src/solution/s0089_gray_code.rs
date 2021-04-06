9f0fdb8021c27b3b842277214efc4e3c7900dd1c
Wei Tan <tanwei0404@163.com>
2021-04-06 08:32:28 +0800

    89. Gray Code: AC

---
+++ src/solution/s0089_gray_code.rs
@@ -0,0 +1,79 @@
+/**
+ * [89] Gray Code
+ *
+ * The gray code is a binary numeral system where two successive values differ in only one bit.
+ * Given an integer n representing the total number of bits in the code, return any sequence of gray code.
+ * A gray code sequence must begin with 0.
+ *  
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: [0,1,3,2]
+ * Explanation:
+ * 00 - 0
+ * 0<u>1</u> - 1
+ * <u>1</u>1 - 3
+ * 1<u>0</u> - 2
+ * [0,2,3,1] is also a valid gray code sequence.
+ * 00 - 0
+ * <u>1</u>0 - 2
+ * 1<u>1</u> - 3
+ * <u>0</u>1 - 1
+ *
+ * Example 2:
+ *
+ * Input: n = 1
+ * Output: [0,1]
+ *
+ *  
+ * Constraints:
+ *
+ * 	1 <= n <= 16
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/gray-code/
+// discuss: https://leetcode.com/problems/gray-code/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn gray_code(n: i32) -> Vec<i32> {
+        let mut x = 1;
+        let mut ret = vec![0];
+
+        for _ in 0..n {
+            let mut rev = ret.iter().rev().map(|&num| num + x).collect();
+            ret.append(&mut rev);
+            x *= 2;
+        }
+
+        ret
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn test_0089_example_1() {
+        let n = 2;
+        let result = vec![0, 1, 3, 2];
+
+        assert_eq!(Solution::gray_code(n), result);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_0089_example_2() {
+        let n = 1;
+        let result = vec![0, 1];
+
+        assert_eq!(Solution::gray_code(n), result);
+    }
+}
