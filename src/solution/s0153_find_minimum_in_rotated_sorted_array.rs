e8e2985b40c02ebc147f18148e15016f459cf858
Wei Tan <tanwei0404@163.com>
2021-06-03 08:53:19 +0800

    153. Find Minimum in Rotated Sorted Array: AC

---
+++ src/solution/s0153_find_minimum_in_rotated_sorted_array.rs
@@ -0,0 +1,96 @@
+/**
+ * [153] Find Minimum in Rotated Sorted Array
+ *
+ * Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+ *
+ * 	[4,5,6,7,0,1,2] if it was rotated 4 times.
+ * 	[0,1,2,4,5,6,7] if it was rotated 7 times.
+ *
+ * Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+ * Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+ * You must write an algorithm that runs in O(log n) time.
+ *  
+ * Example 1:
+ *
+ * Input: nums = [3,4,5,1,2]
+ * Output: 1
+ * Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+ *
+ * Example 2:
+ *
+ * Input: nums = [4,5,6,7,0,1,2]
+ * Output: 0
+ * Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+ *
+ * Example 3:
+ *
+ * Input: nums = [11,13,15,17]
+ * Output: 11
+ * Explanation: The original array was [11,13,15,17] and it was rotated 4 times.
+ *
+ *  
+ * Constraints:
+ *
+ * 	n == nums.length
+ * 	1 <= n <= 5000
+ * 	-5000 <= nums[i] <= 5000
+ * 	All the integers of nums are unique.
+ * 	nums is sorted and rotated between 1 and n times.
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+// discuss: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn find_min(nums: Vec<i32>) -> i32 {
+        let mut cur = std::i32::MAX;
+        let (mut left, mut right) = (0, nums.len() - 1);
+
+        while right >= left {
+            let mid = left + (right - left) / 2;
+            cur = cur.min(nums[mid]);
+            if nums[right] > nums[mid] {
+                right = mid.checked_sub(1).unwrap_or(0);
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        cur
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_0153_example_1() {
+        let nums = vec![3, 4, 5, 1, 2];
+        let result = 1;
+
+        assert_eq!(Solution::find_min(nums), result);
+    }
+
+    #[test]
+    fn test_0153_example_2() {
+        let nums = vec![4, 5, 6, 7, 0, 1, 2];
+        let result = 0;
+
+        assert_eq!(Solution::find_min(nums), result);
+    }
+
+    #[test]
+    fn test_0153_example_3() {
+        let nums = vec![11, 13, 15, 17];
+        let result = 11;
+
+        assert_eq!(Solution::find_min(nums), result);
+    }
+}
