64fc26c1eb90a8363ce0626e03f6415888e0c4d5
Wei Tan <tanwei0404@163.com>
2021-12-02 09:32:39 +0800

    436. Find Right Interval: AC

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -326,3 +326,4 @@ mod s0432_all_oone_data_structure;
 mod s0433_minimum_genetic_mutation;
 mod s0434_number_of_segments_in_a_string;
 mod s0435_non_overlapping_intervals;
+mod s0436_find_right_interval;
