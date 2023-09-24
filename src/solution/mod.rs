05bf4498c393f7ae0427032ec39ec022756bb3f2
Wei Tan <tanwei0404@163.com>
2023-09-24 21:54:02 +0800

    1320. Minimum Distance to Type a Word Using Two Fingers: RETRY, credit: https://leetcode.com/problems/minimum-distance-to-type-a-word-using-two-fingers/solutions/3096968/just-a-runnable-solution/

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -987,3 +987,4 @@ mod s1316_distinct_echo_substrings;
 mod s1317_convert_integer_to_the_sum_of_two_no_zero_integers;
 mod s1318_minimum_flips_to_make_a_or_b_equal_to_c;
 mod s1319_number_of_operations_to_make_network_connected;
+mod s1320_minimum_distance_to_type_a_word_using_two_fingers;
