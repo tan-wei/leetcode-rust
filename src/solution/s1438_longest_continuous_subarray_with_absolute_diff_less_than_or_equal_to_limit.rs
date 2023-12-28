7d1dc8cbb707d76726c914a223a7c005481d0372
Wei Tan <tanwei0404@163.com>
2023-12-28 11:43:11 +0800

    1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit: AC

---
+++ src/solution/s1438_longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.rs
@@ -0,0 +1,117 @@
+/**
+ * [1438] Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+ *
+ * Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
+ *  
+ * Example 1:
+ *
+ * Input: nums = [8,2,4,7], limit = 4
+ * Output: 2
+ * Explanation: All subarrays are:
+ * [8] with maximum absolute diff |8-8| = 0 <= 4.
+ * [8,2] with maximum absolute diff |8-2| = 6 > 4.
+ * [8,2,4] with maximum absolute diff |8-2| = 6 > 4.
+ * [8,2,4,7] with maximum absolute diff |8-2| = 6 > 4.
+ * [2] with maximum absolute diff |2-2| = 0 <= 4.
+ * [2,4] with maximum absolute diff |2-4| = 2 <= 4.
+ * [2,4,7] with maximum absolute diff |2-7| = 5 > 4.
+ * [4] with maximum absolute diff |4-4| = 0 <= 4.
+ * [4,7] with maximum absolute diff |4-7| = 3 <= 4.
+ * [7] with maximum absolute diff |7-7| = 0 <= 4.
+ * Therefore, the size of the longest subarray is 2.
+ *
+ * Example 2:
+ *
+ * Input: nums = [10,1,2,4,7,2], limit = 5
+ * Output: 4
+ * Explanation: The subarray [2,4,7,2] is the longest since the maximum absolute diff is |2-7| = 5 <= 5.
+ *
+ * Example 3:
+ *
+ * Input: nums = [4,2,2,2,4,4,2,2], limit = 0
+ * Output: 3
+ *
+ *  
+ * Constraints:
+ *
+ * 	1 <= nums.length <= 10^5
+ * 	1 <= nums[i] <= 10^9
+ * 	0 <= limit <= 10^9
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/
+// discuss: https://leetcode.com/problems/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn longest_subarray(nums: Vec<i32>, limit: i32) -> i32 {
+        let mut i = 0;
+        let mut minq = vec![];
+        let mut maxq = vec![];
+
+        for num in nums.iter() {
+            while minq.len() > 0 && num < minq.last().unwrap() {
+                minq.pop();
+            }
+
+            while maxq.len() > 0 && num > maxq.last().unwrap() {
+                maxq.pop();
+            }
+
+            minq.push(*num);
+            maxq.push(*num);
+
+            if maxq[0] - minq[0] > limit {
+                if minq[0] == nums[i] {
+                    minq.remove(0);
+                }
+                if maxq[0] == nums[i] {
+                    maxq.remove(0);
+                }
+                i += 1;
+            }
+        }
+
+        (nums.len() as i32) - (i as i32)
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1438_example_1() {
+        let nums = vec![8, 2, 4, 7];
+        let limit = 4;
+
+        let result = 2;
+
+        assert_eq!(Solution::longest_subarray(nums, limit), result);
+    }
+
+    #[test]
+    fn test_1438_example_2() {
+        let nums = vec![10, 1, 2, 4, 7, 2];
+        let limit = 5;
+
+        let result = 4;
+
+        assert_eq!(Solution::longest_subarray(nums, limit), result);
+    }
+
+    #[test]
+    fn test_1438_example_3() {
+        let nums = vec![4, 2, 2, 2, 4, 4, 2, 2];
+        let limit = 0;
+
+        let result = 3;
+
+        assert_eq!(Solution::longest_subarray(nums, limit), result);
+    }
+}
