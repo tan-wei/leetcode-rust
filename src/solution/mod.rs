058b2511f5f45a8bbc4bf1c65961ba2cac2ad68b
Wei Tan <tanwei0404@163.com>
2024-04-09 11:51:11 +0800

    1553. Minimum Number of Days to Eat N Oranges: AC

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1171,3 +1171,4 @@ mod s1547_minimum_cost_to_cut_a_stick;
 mod s1550_three_consecutive_odds;
 mod s1551_minimum_operations_to_make_array_equal;
 mod s1552_magnetic_force_between_two_balls;
+mod s1553_minimum_number_of_days_to_eat_n_oranges;
