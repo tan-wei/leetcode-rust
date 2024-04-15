76edba605eaa3c00258629f6cc2088644eeeac67
Wei Tan <tanwei0404@163.com>
2024-04-15 21:48:34 +0800

    1561. Maximum Number of Coins You Can Get: AC

---
+++ src/solution/s1561_maximum_number_of_coins_you_can_get.rs
@@ -0,0 +1,89 @@
+/**
+ * [1561] Maximum Number of Coins You Can Get
+ *
+ * There are 3n piles of coins of varying size, you and your friends will take piles of coins as follows:
+ *
+ * 	In each step, you will choose any 3 piles of coins (not necessarily consecutive).
+ * 	Of your choice, Alice will pick the pile with the maximum number of coins.
+ * 	You will pick the next pile with the maximum number of coins.
+ * 	Your friend Bob will pick the last pile.
+ * 	Repeat until there are no more piles of coins.
+ *
+ * Given an array of integers piles where piles[i] is the number of coins in the i^th pile.
+ * Return the maximum number of coins that you can have.
+ *  
+ * Example 1:
+ *
+ * Input: piles = [2,4,1,2,7,8]
+ * Output: 9
+ * Explanation: Choose the triplet (2, 7, 8), Alice Pick the pile with 8 coins, you the pile with 7 coins and Bob the last one.
+ * Choose the triplet (1, 2, 4), Alice Pick the pile with 4 coins, you the pile with 2 coins and Bob the last one.
+ * The maximum number of coins which you can have are: 7 + 2 = 9.
+ * On the other hand if we choose this arrangement (1, 2, 8), (2, 4, 7) you only get 2 + 4 = 6 coins which is not optimal.
+ *
+ * Example 2:
+ *
+ * Input: piles = [2,4,5]
+ * Output: 4
+ *
+ * Example 3:
+ *
+ * Input: piles = [9,8,7,6,5,1,2,3,4]
+ * Output: 18
+ *
+ *  
+ * Constraints:
+ *
+ * 	3 <= piles.length <= 10^5
+ * 	piles.length % 3 == 0
+ * 	1 <= piles[i] <= 10^4
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/maximum-number-of-coins-you-can-get/
+// discuss: https://leetcode.com/problems/maximum-number-of-coins-you-can-get/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn max_coins(piles: Vec<i32>) -> i32 {
+        let mut piles = piles;
+        piles.sort_unstable();
+        piles.iter().skip((piles.len() / 3)).step_by(2).sum()
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1561_example_1() {
+        let piles = vec![2, 4, 1, 2, 7, 8];
+
+        let result = 9;
+
+        assert_eq!(Solution::max_coins(piles), result);
+    }
+
+    #[test]
+    fn test_1561_example_2() {
+        let piles = vec![2, 4, 5];
+
+        let result = 4;
+
+        assert_eq!(Solution::max_coins(piles), result);
+    }
+
+    #[test]
+    fn test_1561_example_3() {
+        let piles = vec![9, 8, 7, 6, 5, 1, 2, 3, 4];
+
+        let result = 18;
+
+        assert_eq!(Solution::max_coins(piles), result);
+    }
+}
