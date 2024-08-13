3dc49a3cd3f3adc4181de492259acee308e6e17c
Wei Tan <tanwei0404@163.com>
2024-08-13 12:12:56 +0800

    1642. Furthest Building You Can Reach: AC

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1238,3 +1238,4 @@ mod s1638_count_substrings_that_differ_by_one_character;
 mod s1639_number_of_ways_to_form_a_target_string_given_a_dictionary;
 mod s1640_check_array_formation_through_concatenation;
 mod s1641_count_sorted_vowel_strings;
+mod s1642_furthest_building_you_can_reach;
