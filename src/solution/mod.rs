018e8607b1dbf2fb6ce066fd0efc65e2da85531b
Wei Tan <tanwei0404@163.com>
2025-03-20 23:57:04 +0800

    1928. Minimum Cost to Reach Destination in Time: RETRY

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1453,3 +1453,4 @@ mod s1923_longest_common_subpath;
 mod s1925_count_square_sum_triples;
 mod s1926_nearest_exit_from_entrance_in_maze;
 mod s1927_sum_game;
+mod s1928_minimum_cost_to_reach_destination_in_time;
