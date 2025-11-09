e23f058e8f6e349f70fed72a42ae16a63402ec14
Wei Tan <tanwei0404@163.com>
2025-11-09 13:02:26 +0800

    2231. Largest Number After Digit Swaps by Parity: AC

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1687,3 +1687,4 @@ mod s2224_minimum_number_of_operations_to_convert_time;
 mod s2225_find_players_with_zero_or_one_losses;
 mod s2226_maximum_candies_allocated_to_k_children;
 mod s2227_encrypt_and_decrypt_strings;
+mod s2231_largest_number_after_digit_swaps_by_parity;
