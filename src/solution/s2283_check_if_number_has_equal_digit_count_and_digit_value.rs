cb4179b15e4c0a8b5a90d802af619edb26f2f8b2
Wei Tan <tanwei0404@163.com>
2025-12-21 23:17:41 +0800

    2283. Check if Number Has Equal Digit Count and Digit Value: AC

---
+++ src/solution/s2283_check_if_number_has_equal_digit_count_and_digit_value.rs
@@ -0,0 +1,92 @@
+/**
+ * [2283] Check if Number Has Equal Digit Count and Digit Value
+ *
+ * You are given a 0-indexed string num of length n consisting of digits.
+ * Return true if for every index i in the range 0 <= i < n, the digit i occurs num[i] times in num, otherwise return false.
+ *  
+ * Example 1:
+ *
+ * Input: num = "1210"
+ * Output: true
+ * Explanation:
+ * num[0] = '1'. The digit 0 occurs once in num.
+ * num[1] = '2'. The digit 1 occurs twice in num.
+ * num[2] = '1'. The digit 2 occurs once in num.
+ * num[3] = '0'. The digit 3 occurs zero times in num.
+ * The condition holds true for every index in "1210", so return true.
+ *
+ * Example 2:
+ *
+ * Input: num = "030"
+ * Output: false
+ * Explanation:
+ * num[0] = '0'. The digit 0 should occur zero times, but actually occurs twice in num.
+ * num[1] = '3'. The digit 1 should occur three times, but actually occurs zero times in num.
+ * num[2] = '0'. The digit 2 occurs zero times in num.
+ * The indices 0 and 1 both violate the condition, so return false.
+ *
+ *  
+ * Constraints:
+ *
+ * 	n == num.length
+ * 	1 <= n <= 10
+ * 	num consists of digits.
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value/
+// discuss: https://leetcode.com/problems/check-if-number-has-equal-digit-count-and-digit-value/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn digit_count(num: String) -> bool {
+        let mut hm = std::collections::HashMap::new();
+
+        for c in num.chars() {
+            let d: usize = c.to_digit(10).unwrap_or(88) as usize;
+            *hm.entry(d).or_insert(0) += 1;
+        }
+
+        for (i, c) in num.chars().enumerate() {
+            let cnt: usize = c.to_digit(10).unwrap_or(99) as usize;
+            if !hm.contains_key(&i) {
+                if cnt == 0 {
+                    continue;
+                } else {
+                    return false;
+                }
+            } else if hm[&i] != cnt {
+                return false;
+            }
+        }
+
+        true
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_2283_example_1() {
+        let num = "1210".to_string();
+
+        let result = true;
+
+        assert_eq!(Solution::digit_count(num), result);
+    }
+
+    #[test]
+    fn test_2283_example_2() {
+        let num = "030".to_string();
+
+        let result = false;
+
+        assert_eq!(Solution::digit_count(num), result);
+    }
+}
