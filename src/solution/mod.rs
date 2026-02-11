4e892febd5c34cbf663311a400fafe00f63adc6f
Wei Tan <tanwei0404@163.com>
2026-02-11 22:46:49 +0800

    2351. First Letter to Appear Twice: AC

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1781,3 +1781,4 @@ mod s2347_best_poker_hand;
 mod s2348_number_of_zero_filled_subarrays;
 mod s2349_design_a_number_container_system;
 mod s2350_shortest_impossible_sequence_of_rolls;
+mod s2351_first_letter_to_appear_twice;
