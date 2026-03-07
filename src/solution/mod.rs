9012ad5d7bc620ce4eb82458e7cb19235af446bf
Wei Tan <tanwei0404@163.com>
2026-03-07 17:53:26 +0800

    2383. Minimum Hours of Training to Win a Competition: AC

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1805,3 +1805,4 @@ mod s2379_minimum_recolors_to_get_k_consecutive_black_blocks;
 mod s2380_time_needed_to_rearrange_a_binary_string;
 mod s2381_shifting_letters_ii;
 mod s2382_maximum_segment_sum_after_removals;
+mod s2383_minimum_hours_of_training_to_win_a_competition;
