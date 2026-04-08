4c37063e3f25f4bbd74cd573da85c2bfaea15844
Wei Tan <tanwei0404@163.com>
2026-04-08 22:08:40 +0800

    2423. Remove Letter To Equalize Frequency: RETRY

---
+++ src/solution/s2423_remove_letter_to_equalize_frequency.rs
@@ -0,0 +1,69 @@
+/**
+ * [2423] Remove Letter To Equalize Frequency
+ *
+ * You are given a 0-indexed string word, consisting of lowercase English letters. You need to select one index and remove the letter at that index from word so that the frequency of every letter present in word is equal.
+ * Return true if it is possible to remove one letter so that the frequency of all letters in word are equal, and false otherwise.
+ * Note:
+ *
+ * 	The frequency of a letter x is the number of times it occurs in the string.
+ * 	You must remove exactly one letter and cannot choose to do nothing.
+ *
+ *  
+ * Example 1:
+ *
+ * Input: word = "abcc"
+ * Output: true
+ * Explanation: Select index 3 and delete it: word becomes "abc" and each character has a frequency of 1.
+ *
+ * Example 2:
+ *
+ * Input: word = "aazz"
+ * Output: false
+ * Explanation: We must delete a character, so either the frequency of "a" is 1 and the frequency of "z" is 2, or vice versa. It is impossible to make all present letters have equal frequency.
+ *
+ *  
+ * Constraints:
+ *
+ * 	2 <= word.length <= 100
+ * 	word consists of lowercase English letters only.
+ *
+ */
+pub struct Solution {}
+
+// problem: https://leetcode.com/problems/remove-letter-to-equalize-frequency/
+// discuss: https://leetcode.com/problems/remove-letter-to-equalize-frequency/discuss/?currentPage=1&orderBy=most_votes&query=
+
+// submission codes start here
+
+impl Solution {
+    pub fn equal_frequency(word: String) -> bool {
+        false
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn test_2423_example_1() {
+        let word = "abcc".to_string();
+
+        let result = true;
+
+        assert_eq!(Solution::equal_frequency(word), result);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_2423_example_2() {
+        let word = "aazz".to_string();
+
+        let result = false;
+
+        assert_eq!(Solution::equal_frequency(word), result);
+    }
+}
