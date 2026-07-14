61a876c0765c9ffb8ca5be4b349cdfa86019df03
Wei Tan <tanwei0404@163.com>
2026-07-14 22:59:37 +0800

    2542. Maximum Subsequence Score: RETRY

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1934,3 +1934,4 @@ mod s2537_count_the_number_of_good_subarrays;
 mod s2538_difference_between_maximum_and_minimum_price_sum;
 mod s2540_minimum_common_value;
 mod s2541_minimum_operations_to_make_array_equal_ii;
+mod s2542_maximum_subsequence_score;
