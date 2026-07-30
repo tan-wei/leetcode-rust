6a01fb1cb8017d347dc3169713e3efd5cddb3f8f
Wei Tan <tanwei0404@163.com>
2026-07-30 23:06:32 +0800

    2560. House Robber IV: RETRY

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1950,3 +1950,4 @@ mod s2555_maximize_win_from_two_segments;
 mod s2556_disconnect_path_in_a_binary_matrix_by_at_most_one_flip;
 mod s2558_take_gifts_from_the_richest_pile;
 mod s2559_count_vowel_strings_in_ranges;
+mod s2560_house_robber_iv;
