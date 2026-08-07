925c76625d037b5bcd8b9a4de6527d191913f891
Wei Tan <tanwei0404@163.com>
2026-08-07 21:34:03 +0800

    2568. Minimum Impossible OR: RETRY

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1958,3 +1958,4 @@ mod s2564_substring_xor_queries;
 mod s2565_subsequence_with_the_minimum_score;
 mod s2566_maximum_difference_by_remapping_a_digit;
 mod s2567_minimum_score_by_changing_two_elements;
+mod s2568_minimum_impossible_or;
