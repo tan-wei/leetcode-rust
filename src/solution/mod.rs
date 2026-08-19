d37a046a8b6bf6145c3f7d3c3d31acde123e7493
Wei Tan <tanwei0404@163.com>
2026-08-19 21:10:45 +0800

    2580. Count Ways to Group Overlapping Ranges: RETRY

--- src/solution/mod.rs
+++ src/solution/mod.rs
@@ -1970,3 +1970,4 @@ mod s2576_find_the_maximum_number_of_marked_indices;
 mod s2577_minimum_time_to_visit_a_cell_in_a_grid;
 mod s2578_split_with_minimum_sum;
 mod s2579_count_total_number_of_colored_cells;
+mod s2580_count_ways_to_group_overlapping_ranges;
